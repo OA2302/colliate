c6e699e5112492b327468bc76a3f026b7b6c8340
OA2302 <okikiadenekan3@gmail.com>
2023-10-20 21:17:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+function showSchedule1() {
+    document.getElementById("schdl2").innerHTML = `
+    <h2>
+        SCHEDULE
+    </h2>
+    <table>
+        <tr>
+            <td>
+                25 Nov 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+        <tr>
+            <td>
+                28 Nov 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+        <tr>
+            <td>
+                18 Dec 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+        <tr>
+            <td>
+                7 Jan 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+    </table>
+
+    `  
+}
+function showSchedule2() {
+    document.getElementById("schdl2").innerHTML = `
+    <h2>
+        SCHEDULE
+    </h2>
+    <table>
+        <tr>
+            <td>
+                30 Nov 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+        <tr>
+            <td>
+                1 dec 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+        <tr>
+            <td>
+                25 Dec 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+        <tr>
+            <td>
+                1 Jan 2023
+            </td>
+            <td>
+                Vestibulum viverra
+            </td>
+        </tr>
+    </table>
+
+    `  
+}
